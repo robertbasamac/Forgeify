0afd28fd18bbdfc188098110a39cef2624ae62d8
Robert Basamac <rbasamac@gmail.com>
2023-09-27 16:09:19 +0300

    Project creation

---
+++ Forgeify/ForgeifyApp.swift
@@ -0,0 +1,17 @@
+//
+//  ForgeifyApp.swift
+//  Forgeify
+//
+//  Created by Robert Basamac on 27.09.2023.
+//
+
+import SwiftUI
+
+@main
+struct ForgeifyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
